427e5e9654cf6deaa926eee9bbf3c2578aa12466
foyojhumel <foyojhumel24@gmail.com>
2026-04-05 18:15:35 +0800

    Add second solution to problem 3 using python. Using while loop has longer execution time, inefficient algorithm.

--- problem_0003_longest_substring_without_repeating_characters/solution_0003_longest_substring_without_repeating_characters.py
+++ problem_0003_longest_substring_without_repeating_characters/solution_0003_longest_substring_without_repeating_characters.py
@@ -24,3 +24,46 @@ class Solution:
                 countlist.append(count)
 
             return max(countlist)
+
+# Second solution
+#  no nested for loop, but a much more longer execution time with while loop
+# Execution time = 1685ms, Memory size = 19.5mb
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        #the goal is to return the longest chain of non-repeating characters
+        #the possible longest will be 26, the count of English alphabets
+        #there are many possible chain of non-repeating characters, we are
+        # only to return the maximum count
+
+        #if list contains one or no character, return its length
+        if (len(s) < 2):
+            return len(s)
+
+        #count the letters seen during check of all letters in the string
+        letter_count = 0
+
+        #remember the longest count seen so far
+        lengthiest = 0
+
+        #count of step up to end of string
+        count = 1
+
+        #continuous check while not seeing the whole string
+        while ((count != len(s)) and (lengthiest < len(s[letter_count:]))):
+            #check if the current letter is in the preceeding substring
+            if s[count] not in s[letter_count:count]:
+                count += 1
+                if (count == len(s)):
+                    return len(s[letter_count:])
+            else:
+                #if repeating character is found in the substring, compute the
+                # length
+                current_length = count - letter_count
+                if current_length > lengthiest:
+                    lengthiest = current_length
+                #add the count value to letter_count, to track the number of
+                # letters seen
+                letter_count += 1
+                count = letter_count + 1
+        
+        return lengthiest
